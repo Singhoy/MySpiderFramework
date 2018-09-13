6cdddd643bf94dd9e7494a094bb7e2820b091129
Singhoi <380309472@qq.com>
2018-09-13 17:06:36 +0800

    在引擎中添加中间件

--- scrapy_plus/core/engine.py
+++ scrapy_plus/core/engine.py
@@ -1,5 +1,7 @@
 """引擎组件"""
 from _http.request import Request
+from middlewares.downloader_middlewares import DownloaderMiddleware
+from middlewares.spider_middlewares import SpiderMiddleware
 from .pipeline import Pipeline
 from .downloader import Downloader
 from .scheduler import Scheduler
@@ -17,6 +19,8 @@ class Engine(object):
         self.scheduler = Scheduler()
         self.downloader = Downloader()
         self.pipeline = Pipeline()
+        self.spider_middleware = SpiderMiddleware()
+        self.downloader_middleware = DownloaderMiddleware()
 
     def start(self):
         """启动引擎，对外提供接口"""
@@ -27,14 +31,20 @@ class Engine(object):
         # 爬虫起始请求
         start_request = self.spider.start_request()
 
-        # 把起始请求添加给调度器
+        # 利用爬虫中间件预处理请求对象
+        start_request = self.spider_middleware.process_request(start_request)
+        # 把请求添加给调度器
         self.scheduler.add_request(start_request)
 
         # 从调度器获取请求对象，交给下载器发起请求，获取一个响应对象
         request = self.scheduler.get_request()
 
+        # 利用下载器中间件预处理请求对象
+        request = self.downloader_middleware.process_request(request)
         # 利用下载器发起请求
         response = self.downloader.get_response(request)
+        # 利用下载器中间件预处理响应对象
+        response = self.downloader_middleware.process_response(response)
 
         # 利用爬虫的解析响应方法，处理响应，得到结果
         result = self.spider.parse(response)
@@ -42,6 +52,8 @@ class Engine(object):
         # 判断结果对象
         if isinstance(result, Request):
             # 如果是请求对象，就再交给调度器
+            # 利用爬虫中间件预处理请求对象
+            result = self.spider_middleware.process_request(result)
             self.scheduler.add_request(result)
         else:
             # 否则，交给管道处理
