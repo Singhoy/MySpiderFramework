61eeb1b55a2bd0aae2315f08a255f537cc7fd318
Singhoi <380309472@qq.com>
2018-09-14 19:12:16 +0800

    测试动态导入配置效果

--- project_dir/settings.py
+++ project_dir/settings.py
@@ -1,1 +1,26 @@
+"""项目中的配置信息"""
 DEFAULT_LOG_FILENAME = '日志.log'
+
+# 配置爬虫
+SPIDERS = [
+    'spiders.baidu.BaiduSpider',
+    'spiders.douban.DoubanSpider',
+]
+
+# 配置管道
+PIPELINES = [
+    'pipelines.BaiduPipeline',
+    'pipelines.DoubanPipeline',
+]
+
+# 配置下载器中间件
+DOWNLOADER_MIDDLEWARES = [
+    'middlewares.downloader_middlewares.DownloaderMiddleware1',
+    'middlewares.downloader_middlewares.DownloaderMiddleware2',
+]
+
+# 配置爬虫中间件
+SPIDER_MIDDLEWARES = [
+    'middlewares.spider_middlewares.SpiderMiddleware1',
+    'middlewares.spider_middlewares.SpiderMiddleware2',
+]
